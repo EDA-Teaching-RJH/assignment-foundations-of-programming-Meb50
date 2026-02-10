66613d8c639db03a1e559916d62ba84d9d728658
Meb50 <meb50@kent.ac.uk>
2026-02-10 18:39:21 +0000

    begin

---
+++ fleet_manager.py
@@ -0,0 +1,25 @@
+n = []
+r = []
+d = []
+id = []
+
+
+def init_database():
+
+display_menu():
+
+add_member(names, ranks, divs, ids):
+
+remove_member(names, ranks, divs, ids):
+
+update_rank(names, ranks, ids):
+
+display_roster(names, ranks, divs, ids):
+
+search_crew(names, ranks, divs, ids):
+
+filter_by_division(names, divs):
+
+calculate_payroll(ranks):
+
+count_officers(ranks):
